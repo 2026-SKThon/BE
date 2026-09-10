d11f1670b985d5e02ca1efc6d9f0b8d0eb2173a5
Chungs0604 <alsk3528@gmail.com>
2026-09-10 22:54:24 +0900

    🗃️ Comment: healthController 로그 추가

--- src/main/java/com/one/global/util/HealthCheckController.java
+++ src/main/java/com/one/global/util/HealthCheckController.java
@@ -1,14 +1,17 @@
 package com.one.global.util;
 
 import com.one.global.response.BaseResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class HealthCheckController {
 
     @GetMapping("/health")
     public BaseResponse<String> health() {
+        log.info("[healthController] 성공");
         return BaseResponse.success("ok");
     }
 }
